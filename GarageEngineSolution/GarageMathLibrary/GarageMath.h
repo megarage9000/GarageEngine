c43c8bb8b95351098df3aa49eea7d9e9ade90cbf
John <gtom_1984@hotmail.ca>
2025-07-03 15:06:31 -0700

    Finish operators for vector3

--- GarageEngineSolution/GarageMathLibrary/GarageMath.h
+++ GarageEngineSolution/GarageMathLibrary/GarageMath.h
@@ -15,6 +15,7 @@ namespace GarageLinearAlgebra {
 
 	// constexpr operations
 
+
 	// --- Vector4 ---- 
 	inline Vector4 operator* (const float& scalar, const Vector4& vector) {
 		return Vector4 {
@@ -44,6 +45,25 @@ namespace GarageLinearAlgebra {
 	inline Vec4 operator-(const Vector4& vectorA, const Vector4& vectorB) {
 		Vector4 res;
 		subtract_vectors(vectorA.data(), vectorB.data(), res.data(), 4);
+		return res;
+	}
+
+	// --- Vector3 ---
+	inline Vector3 operator* (const float& scalar, const Vector3& vector) {
+		return Vector3{
+			vector[0] * scalar,
+			vector[1] * scalar,
+			vector[2] * scalar,
+		};
+	}
+
+	inline Vector3 operator* (const Vector3& vector, const float& scalar) {
+		return scalar * vector;
+	}
+
+	inline Matrix3 operator* (const Vector3& vectorA, const Vector3& vectorB) {
+		Matrix3 res;
+		multiply_vectors(vectorA.data(), vectorB.data(), res.data(), 3, 9);
 		return res;
 	}
 }
